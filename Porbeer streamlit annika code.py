b690795dab06440e22f7b79729f5ac3d2f2efefc
annikaharts <114263741+annikaharts@users.noreply.github.com>
2022-10-13 15:06:34 +0200

    Update Porbeer streamlit annika code.py

--- Porbeer streamlit annika code.py
+++ Porbeer streamlit annika code.py
@@ -1,8 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
+st.subheader('Laadpaaldata') 
+st.text("Om de date op te schonen en overzichtelijk te maken hebben we de volgende bewerkingen uitgevoerd:")
+st.text("Allereerst hebben we alle NA-waardes verwijderd.")
+st.text("Vervolgens hebben we alle datums omgezet naar datetime format.")
+st.text("Toen hebben we gegekeken of de aangesloten uren volgens de Ended-Started time wel overeen kwamen,")
+st.text("hierbij waren er 5 erg afwijkend, dus hebben we deze verwijderd.")
+st.text("Hevige 'uitschieters' hebben we verwijderd om de meest voorkomende oplaadtijden te kunnen plotten voor een beter overzicht")
+st.text("De gemiddelde en mediaan zijn berekend om te implementeren in de volgende figuren.")
+st.text("")
 
 # !pip install streamlit
 import streamlit as st
@@ -31,14 +35,14 @@ laadpaaldata['ConnectedAangeslotenDif'] = laadpaaldata['ConnectedTime'] - laadpa
 # De 5 waardes waarbij het verschil in ChargeTime en AangeslotenTime daadwerkelijk meet dan 0,1 uur verschilt
 laadpaaldata = laadpaaldata[((laadpaaldata['ConnectedAangeslotenDif'] < 0.1) & (laadpaaldata['ConnectedAangeslotenDif'] > 0)) | ((laadpaaldata['ConnectedAangeslotenDif'] > -0.1) & (laadpaaldata['ConnectedAangeslotenDif'] < 0))]
 
-# Uitschieters lengte van laadsessie, de aansluiting, eruit halen
+# Hele lange tijden van laadsessie, de aansluiting, eruit halen
 q_laad = laadpaaldata['LaadsessieAangeslotenUren'].quantile(0.965)
 laadpaaldata[laadpaaldata['LaadsessieAangeslotenUren']<q_laad]
 q_low_laad = laadpaaldata['LaadsessieAangeslotenUren'].quantile(0.035)
 q_hi_laad = laadpaaldata['LaadsessieAangeslotenUren'].quantile(0.965)
 laadpaaldata = laadpaaldata[(laadpaaldata["LaadsessieAangeslotenUren"] < q_hi_laad) & (laadpaaldata["LaadsessieAangeslotenUren"] > q_low_laad)]
 
-# Uitschieters lengte van chargetime eruit halen
+# Hele lange tijden van chargetime eruit halen
 q_charge = laadpaaldata['ChargeTime'].quantile(0.965)
 laadpaaldata[laadpaaldata['ChargeTime']<q_charge]
 q_low_charge = laadpaaldata['ChargeTime'].quantile(0.35)
@@ -58,6 +62,10 @@ gemiddelde = round(laadpaaldata['ChargeTime'].mean(), 4)
 mediaan = round(laadpaaldata['ChargeTime'].median(), 4)
 print('Gemiddelde: ' +str(int(gemiddelde))+ " uur en " +str(round((gemiddelde-int(gemiddelde))*60)) +" minuten")
 print('Mediaan: ' +str(int(mediaan))+ " uur en " +str(round((mediaan-int(mediaan))*60)) +" minuten")
+
+st.text('In onderstaand figuur is de verdeling te zien van de tijd dat een auto oplaadt.')
+st.text('Het gemiddelde en de mediaan verschillen weinig. Wel is er duidelijk te zien dat zich twee toppen vormen,')
+st.text("een bimodale normale verdeling waarschijnlijk. De meeste auto's laden iets meer dan 2 uur op.")
 
 fig = px.histogram(laadpaaldata, x="ChargeTime", nbins=40, text_auto=True,
             title='Aantal laadpalen per oplaadtijd',
@@ -91,6 +99,20 @@ fig.add_annotation(x=5.97, y=680,
             bgcolor="#27285C",
             opacity=0.8)
 
+fig.add_annotation(x=5.93, y=610,
+            text='Kansdichtheid: Bimodale verdeling / tweetoppige normale verdeling',
+            showarrow=False,
+            arrowhead=1,
+            align="center",
+            font=dict(color="#ffffff"),
+            ax=20,
+            ay=-30,
+            bordercolor="#ffffff",
+            borderwidth=2,
+            borderpad=4,
+            bgcolor="#27285C",
+            opacity=0.8)
+
 fig.update_layout(yaxis_title="Aantal laadpalen")
 fig.show()
 st.plotly_chart(fig)
@@ -100,6 +122,9 @@ import calendar
 laadtijd_per_maand = laadpaaldata.groupby('Maand')['ChargeTime', "ConnectedTime"].sum().reset_index()
 laadtijd_per_maand['Maand'] = laadtijd_per_maand['Maand'].astype(int)
 laadtijd_per_maand['Maand'] = laadtijd_per_maand['Maand'].apply(lambda x: calendar.month_abbr[x])
+
+st.text('In onderstaand figuur is een duidelijk verschil te zien in de tijd van aansluiten en daadwerkelijk opladen.')
+st.text("De auto's staan in totaal vaak twee keer zo lang aangesloten als dat ze aan het opladen zijn.")
 
 fig = go.Figure()
 fig.add_trace(go.Bar(x=laadtijd_per_maand["Maand"], y=laadtijd_per_maand["ChargeTime"], name="Oplaadtijd"))
